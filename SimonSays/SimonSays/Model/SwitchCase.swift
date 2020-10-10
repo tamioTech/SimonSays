3fe4b4f361747d892395f18c726eadc0147cc689
tamioTech <62820237+tamioTech@users.noreply.github.com>
2020-10-10 16:51:39 -0700

    - start from scratch to make constraints work - add buttons, names and scores - link buttons, names and scores

---
+++ SimonSays/SimonSays/Model/SwitchCase.swift
@@ -0,0 +1,16 @@
+//
+//  SwitchCase.swift
+//  SimonSays
+//
+//  Created by Tyler Hikida on 2020-10-10.
+//
+
+import Foundation
+
+struct SwitchCase {
+    
+    
+    func printTag(senderTag: Int) -> String {
+        return "42"
+    }
+}
